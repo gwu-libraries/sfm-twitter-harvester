e07d6f42f3d899d2eb5237d314eb6ecfc7e8558e
Justin Littman <justinlittman@gwu.edu>
2016-05-03 23:12:26 -0400

    Refs sfm 218. Changes for streams.

--- twitter_stream_warc_iter.py
+++ twitter_stream_warc_iter.py
@@ -2,14 +2,24 @@
 
 from __future__ import absolute_import
 from sfmutils.warc_iter import BaseWarcIter
+from dateutil.parser import parse as date_parse
 
 
 class TwitterStreamWarcIter(BaseWarcIter):
+    def __init__(self, filepaths, limit_user_ids=None):
+        BaseWarcIter.__init__(self, filepaths)
+        self.limit_user_ids = limit_user_ids
+
     def _select_record(self, url):
         return url.startswith("https://stream.twitter.com/1.1")
 
     def _item_iter(self, url, json_obj):
-        yield "twitter_status", json_obj
+        # Only want statuses, not deletes, stall_warnings, etc.
+        # TODO: Support returning other message types.
+        if "id_str" in json_obj:
+            yield "twitter_status", json_obj["id_str"], date_parse(json_obj["created_at"]), json_obj
+        else:
+            yield "twitter_status", None, None, None
 
     @staticmethod
     def item_types():
@@ -18,6 +28,12 @@ class TwitterStreamWarcIter(BaseWarcIter):
     @property
     def line_oriented(self):
         return True
+
+    def _select_item(self, item):
+        if not self.limit_user_ids or item.get("user", {}).get("id_str") in self.limit_user_ids:
+            return True
+        return False
+
 
 if __name__ == "__main__":
     TwitterStreamWarcIter.main(TwitterStreamWarcIter)
