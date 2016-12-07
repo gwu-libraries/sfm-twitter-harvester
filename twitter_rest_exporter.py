6c8d4afb6d3f4d583b6456af16d5bc9d074fb3f7
Dan Kerchner <kerchner@gwu.edu>
2016-12-07 17:08:17 -0500

    Improved organization of export columns; algined updates in data dictionary (ref https://github.com/gwu-libraries/sfm-ui/pull/581).

--- twitter_rest_exporter.py
+++ twitter_rest_exporter.py
@@ -15,34 +15,35 @@ class BaseTwitterStatusTable(BaseTable):
     """
     PETL Table for Twitter statuses.
     """
-    def __init__(self, warc_paths, dedupe, item_date_start, item_date_end, seed_uids, warc_iter_cls,
-                 segment_row_size):
-        BaseTable.__init__(self, warc_paths, dedupe, item_date_start, item_date_end, seed_uids, warc_iter_cls,
-                          segment_row_size)
+    def __init__(self, warc_paths, dedupe, item_date_start, item_date_end,
+                 seed_uids, warc_iter_cls, segment_row_size):
+        BaseTable.__init__(self, warc_paths, dedupe, item_date_start,
+                           item_date_end, seed_uids, warc_iter_cls,
+                           segment_row_size)
 
     def _header_row(self):
-        return ('created_at', 'twitter_id',
-                'screen_name', 'followers_count', 'friends_count',
-                'retweet_count', 'hashtags', 'in_reply_to_screen_name',
-                'twitter_url', 'coordinates', 'text', 'is_retweet', 'is_quote','mentions','favorite_count/like_count',
+        return ('created_at', 'twitter_id', 'screen_name', 'followers_count',
+                'friends_count', 'favorite_count/like_count', 'retweet_count',
+                'hashtags', 'mentions', 'in_reply_to_screen_name',
+                'twitter_url', 'text', 'is_retweet', 'is_quote', 'coordinates',
                 'url1', 'url1_expanded', 'url2', 'url2_expanded', 'media_url')
 
     def _row(self, item):
-        row = [date_parse(item["created_at"]),
-               item["id_str"],
+        row = [date_parse(item['created_at']),
+               item['id_str'],
                item['user']['screen_name'],
                item['user']['followers_count'],
                item['user']['friends_count'],
+               item['favorite_count'],
                item['retweet_count'],
                ', '.join([hashtag['text'] for hashtag in item['entities']['hashtags']]),
+               ', '.join([user_mentions['screen_name'] for user_mentions in item['entities']['user_mentions']]),
                item['in_reply_to_screen_name'] or '',
-               'http://twitter.com/{}/status/{}'.format(item["user"]["screen_name"], item["id_str"]),
-               str(item['coordinates']['coordinates']) if item['coordinates'] else '',
+               'http://twitter.com/{}/status/{}'.format(item['user']['screen_name'], item['id_str']),
                item['text'].replace('\n', ' '),
                'Yes' if 'retweeted_status' in item else 'No',
                'Yes' if 'quoted_status_id' in item else 'No',
-               ', '.join([user_mentions['screen_name'] for user_mentions in item['entities']['user_mentions']]),
-               item['favorite_count']
+               str(item['coordinates']['coordinates']) if item['coordinates'] else ''
                ]
         # only show up to two urls w/expansions
         urlslist = []
@@ -61,16 +62,23 @@ class BaseTwitterStatusTable(BaseTable):
 
 
 class TwitterRestStatusTable(BaseTwitterStatusTable):
-    def __init__(self, warc_paths, dedupe, item_date_start, item_date_end, seed_uids, segment_row_size=None):
-        BaseTwitterStatusTable.__init__(self, warc_paths, dedupe, item_date_start, item_date_end, seed_uids,
-                                        TwitterRestWarcIter, segment_row_size)
+    def __init__(self, warc_paths, dedupe, item_date_start, item_date_end,
+                 seed_uids, segment_row_size=None):
+        BaseTwitterStatusTable.__init__(self, warc_paths, dedupe,
+                                        item_date_start, item_date_end,
+                                        seed_uids, TwitterRestWarcIter,
+                                        segment_row_size)
 
 
 class TwitterRestExporter(BaseExporter):
-    def __init__(self, api_base_url, working_path, mq_config=None, warc_base_path=None):
-        BaseExporter.__init__(self, api_base_url, TwitterRestWarcIter, TwitterRestStatusTable, working_path,
-                              mq_config=mq_config, warc_base_path=warc_base_path)
+    def __init__(self, api_base_url, working_path, mq_config=None,
+                 warc_base_path=None):
+        BaseExporter.__init__(self, api_base_url, TwitterRestWarcIter,
+                              TwitterRestStatusTable, working_path,
+                              mq_config=mq_config,
+                              warc_base_path=warc_base_path)
 
 
 if __name__ == "__main__":
-    TwitterRestExporter.main(TwitterRestExporter, QUEUE, [SEARCH_ROUTING_KEY, TIMELINE_ROUTING_KEY])
+    TwitterRestExporter.main(TwitterRestExporter, QUEUE,
+                             [SEARCH_ROUTING_KEY, TIMELINE_ROUTING_KEY])
