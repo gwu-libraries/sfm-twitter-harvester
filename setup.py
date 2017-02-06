f7aec18540461773acb5c29cffd687cc1c7bff4d
Justin Littman <justinlittman@gwu.edu>
2017-02-06 09:45:37 -0500

    Bumped version to 1.5.0 and set warcprox to 0.3.2.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='sfmtwitterharvester',
-    version='1.4.3',
+    version='1.5.0',
     url='https://github.com/gwu-libraries/sfm-twitter-harvester',
     author='Justin Littman',
     author_email='justinlittman@gmail.com',
