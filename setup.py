89637b3faeb3d67233fb2c099954530252b6572d
Justin Littman <justinlittman@gwu.edu>
2017-10-23 11:44:27 -0400

    Bumped version to 1.11.0.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='sfmtwitterharvester',
-    version='1.10.0',
+    version='1.11.0',
     url='https://github.com/gwu-libraries/sfm-twitter-harvester',
     author='Justin Littman',
     author_email='justinlittman@gmail.com',
