868e6c5f125273adf8a9cb00da5d4e60c8227f1e
Justin Littman <justinlittman@gwu.edu>
2018-08-07 13:46:54 -0400

    Bumped version to 2.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='sfmtwitterharvester',
-    version='2.0.0',
+    version='2.0.1',
     url='https://github.com/gwu-libraries/sfm-twitter-harvester',
     author='Social Feed Manager',
     author_email='sfm@gwu.edu',
