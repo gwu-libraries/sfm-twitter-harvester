0aacf40ef59097af118e3c3f48b8ae41e3e952e8
Laura Wrubel <lwrubel@gwu.edu>
2021-05-21 15:57:15 +0000

    Rebase and fixes to dirs.

--- docker/rest-harvester/invoke.sh
+++ docker/rest-harvester/invoke.sh
@@ -4,7 +4,7 @@ set -e
 sh /opt/sfm-setup/setup_reqs.sh
 
 echo "Waiting for dependencies"
-appdeps.py --wait-secs 60 --port-wait ${SFM_RABBITMQ_HOST}:${SFM_RABBITMQ_PORT} --file-wait /sfm-collection-set-data/collection_set --file-wait /sfm-collection-set-data/containers
+appdeps.py --wait-secs 60 --port-wait ${SFM_RABBITMQ_HOST}:${SFM_RABBITMQ_PORT} --file-wait /sfm-collection-set-data/collection_set --file-wait /sfm-containers-data/containers
 
 echo "Starting harvester"
 exec gosu sfm python twitter_harvester.py --debug=$DEBUG --debug-warcprox=$DEBUG_WARCPROX service $SFM_RABBITMQ_HOST $SFM_RABBITMQ_USER $SFM_RABBITMQ_PASSWORD /sfm-containers-data/containers/$HOSTNAME --tries=$HARVEST_TRIES --priority-queues=$PRIORITY_QUEUES
