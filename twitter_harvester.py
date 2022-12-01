9f5927ab408a4c063acf87332d21b336c10f9373
Adhithya Kiran <adhithyakiran@gwdev-kiran.wrlc.org>
2022-12-01 13:45:40 -0500

    commits after restoring deletions

--- twitter_harvester.py
+++ twitter_harvester.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from __future__ import absolute_import
-from datetime import datetime
+from datetime import datetime, timedelta, timezone
 import logging
 import re
 import json
@@ -10,6 +10,7 @@ import requests
 import threading #stream code
 
 from twarc import Twarc, Twarc2
+from twarc.decorators2 import _snowflake2millis, _millis2date
 from sfmutils.harvester import BaseHarvester, Msg
 from twitter_stream_warc_iter import TwitterStreamWarcIter
 from twitter_stream_warc_iter import TwitterStreamWarcIter2
@@ -27,6 +28,29 @@ TIMELINE2_ROUTING_KEY = "harvest.start.twitter2.twitter_user_timeline_2"
 ACADEMIC_SEARCH_ROUTING_KEY = "harvest.start.twitter2.twitter_academic_search"
 
 status_re = re.compile("^https://twitter.com/.+/status/\d+$")
+
+def since_id_to_timestamp(since_id):
+    '''
+    Convert a Twitter since_id to a tz-aware timestamp.
+    :param since_id: an integer
+    '''
+    since_dt = _millis2date(_snowflake2millis(since_id))
+    # Need to make this timestamp TZ aware (on the assumption that it's UTC to begin with)
+    return since_dt.replace(tzinfo=timezone.utc)
+
+def check_timedelta(timestamp, offset_days=7):
+    '''
+    Given a UTC timestamp, check its validity vis-a-vis the allowed window.
+    :param timsetamp: a tz-aware datetime object in UTC timezone
+    :param offset_days: the number of days prior to the current moment to check against the since_id
+    '''
+    today = datetime.now(timezone.utc)
+    delta = timedelta(days=offset_days)
+    # Is the timestamp more recent than offset_days?
+    if today - delta <= timestamp:
+        return timestamp
+    else:
+        return None
 
 class TwitterHarvester(BaseHarvester):
     def __init__(self, working_path, stream_restart_interval_secs=30 * 60, mq_config=None, debug=False,
@@ -51,7 +75,7 @@ class TwitterHarvester(BaseHarvester):
             self.search_2()
         elif harvest_type == "twitter_academic_search":
             self._create_twarc2()
-            self.search_2()
+            self.search_2(harvest_type=harvest_type)
         elif harvest_type == "twitter_filter":
             self._create_twarc()
             self.filter()
@@ -108,7 +132,12 @@ class TwitterHarvester(BaseHarvester):
         query, geocode = self._search_parameters()
         self._harvest_tweets(self.twarc.search(query, geocode=geocode, since_id=since_id))
     
-    def search_2(self):
+
+    def search_2(self, harvest_type="twitter_search_2"):
+        '''
+        :param harvest_type: str of the harvest type (to differentiate behaviors between search_recent and search_all (Academic Search))
+        '''
+
         assert len(self.message.get("seeds", [])) == 1
 
         incremental = self.message.get("options", {}).get("incremental", False)
@@ -116,6 +145,19 @@ class TwitterHarvester(BaseHarvester):
         since_id = self.state_store.get_state(__name__,
                                               u"{}.since_id".format(self._search_id())) if incremental else None
         query, geocode, start_time, end_time, limit = self._search_parameters2()
+        # Checks necessary for the search_recent endpoint
+        if (harvest_type == "twitter_search_2"):
+            # If since_id is older than 7 days, ignore
+            if since_id and not check_timedelta(since_id_to_timestamp(since_id)):
+                since_id = None
+            # Ignore the start_time param if outside the window for recent searches
+            # Twitter seems to just ignore the end_time if outside the window
+            if start_time:
+                start_time = check_timedelta(start_time)
+        # v.2 API does not allow the conjunction of start/end_time params with since_id
+        # Let since_id take precedence
+        if since_id:
+            start_time, end_time = None, None
         if self.message.get("options").get("twitter_academic_search", False):
             if geocode is not None:
                 query = "".join([query, " point_radius:[", geocode,"]"])
@@ -418,6 +460,7 @@ class TwitterHarvester(BaseHarvester):
         return "not found or deleted"
 
     def _harvest_tweets(self, tweets):
+        # max_tweet_id = None
         for count, tweet in enumerate(tweets):
             if not count % 100:
                 log.debug("Harvested %s tweets", count)
@@ -427,23 +470,26 @@ class TwitterHarvester(BaseHarvester):
                 break
 
     def _harvest_tweets_2(self, tweets, limit=None):
-        for page in tweets:
-            if 'data' not in page:
-                return
-            for count, tweet in enumerate(page['data']):
-                if not count % 100:                    
-                    log.debug("Harvested %s tweets", count)
-                self.result.harvest_counter["tweets"] += 1
-                if limit and self.result.harvest_counter["tweets"] >= limit:
-                    log.debug("Stopping since limit reached.")
-                    self.stop_harvest_seeds_event.set()
-                    #break
-                if self.stop_harvest_seeds_event.is_set():
-                    log.debug("Stopping since stop event set.")
-                    break
-            else:
-                continue  
-            break
+        # max_tweet_id = None
+        # Counter for paginated tweets
+        for i, page in enumerate(tweets):        
+            for page in tweets:
+                if 'data' not in page:
+                    return
+                for count, tweet in enumerate(page['data']):
+                    if not (count + 1) % 100:
+                        log.debug("Harvested %s tweets", (count + 1) * (i + 1))
+                    self.result.harvest_counter["tweets"] += 1
+                    if limit and self.result.harvest_counter["tweets"] >= limit:
+                        log.debug("Stopping since limit reached.")
+                        self.stop_harvest_seeds_event.set()
+                        #break
+                    if self.stop_harvest_seeds_event.is_set():
+                        log.debug("Stopping since stop event set.")
+                        break
+                else:
+                    continue  
+                break
 
 
     # for stream
